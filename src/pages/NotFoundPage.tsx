8f77156ede5c4db8db55a48adc6d3948db425fac
ThierryKriebs <kriebs.thierry@gmail.com>
2025-08-18 16:53:44 +0200

    test deploiement

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,7 @@
 export default function NotFoundPage() {
   return (
     <div className="not-found">
-      <h1>Erreur 404 : page non trouvée</h1>
+      <h1>Erreur 404 : page non  trouvée</h1>
     </div>
   );
 }
